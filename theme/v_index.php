c83a7d13b59cf285f8f5a7b8415cbff70a5550fd
DanJA <peregar2006@gmail.com>
2014-10-07 10:46:12 +0200

    fixes headers, comments the line 15 in the article.php

--- theme/v_index.php
+++ theme/v_index.php
@@ -4,23 +4,23 @@
 <head>
 	<title>Просмотр всех статей</title>
 	<meta content="text/html; charset=utf-8" http-equiv="content-type">
-	<link rel="stylesheet" type="text/css" src="./style.css">
+	<link rel="stylesheet" href="../blog1/theme/style.css" type="text/css" media="screen">
 </head>
 <body>
 <h1>Главная</h1>
-<a href="../blog1/index.php">Главная</a>&nbsp;
+<a href="../blog1/index.php">Главная</a>&nbsp;|
 <a href="../blog1/editor.php">Консоль редактора</a>
 <hr>
-	<ul>
+	
 		<? foreach ($articles as $a): ?>
-			<li>
+			<div id="art-border">
 				<a href="./article.php?id=<?=$a['article_id']?>">
 					<h3><?=$a['title']?></h3>
 				</a>
-				<p><?=$a['content']?></p>	
-			</li>
+				<p style="width: 70%;"><?=$a['content']?></p>	
+			</div>
 		<? endforeach; ?>
 	
-	</ul>
+	
 </body>
 </html>

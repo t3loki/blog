8855740c3851ba56cda3146556d88c075a0f7d40
DanJA <peregar2006@gmail.com>
2014-10-17 18:46:34 +0200

    teaser feature implemented. teasers are shown on the home page.

--- theme/v_edit.php
+++ theme/v_edit.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Изменение статьи</title>
+	<title>Modifying an article</title>
 	<meta content="text/html; charset=utf-8" http-equiv="content-type">
 	<link rel="stylesheet" type="text/css" media="screen" href="theme/style.css" />	
 </head>
 <body>
-<h1>Изменение статьи</h1>
+<h1>Modify an article</h1>
 
 	<!-- главное меню -->
-	<a href="../blog1/index.php">Главная</a>&nbsp;|&nbsp;
-	<a href="../blog1/editor.php">Консоль редактора</a>
+	<a href="../blog1/index.php">Home</a>&nbsp;|&nbsp;
+	<a href="../blog1/editor.php">Editor</a>
 	<hr/>
 	
-	<!-- <? if($error) :?>
-		<b style="color: red;">Заполните все поля!</b>
-	<? endif ?>  -->
+	<? if($error) :?>
+		<b style="color: red;">Fill in all the fields!</b>
+	<? endif ?>  
 
 	<form method="post">
-		Название:
+		Title:
 		<br/>
 		<input type="text" name="title" value="<?=$a[1]?>" />
 		<br/>
 		<br/>
-		Содержание:
+		Content:
 		<br/>
 		<textarea name="content"><?=$a[2]?></textarea>
 		<br/>
-		<input type="submit" value="Изменить" />
+		<input type="submit" value="Modify" />
 	</form>
 </body>
 </html>
--- theme/v_index.php
+++ theme/v_index.php
@@ -2,14 +2,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Просмотр всех статей</title>
+	<title>Viewing all articles</title>
 	<meta content="text/html; charset=utf-8" http-equiv="content-type">
 	<link rel="stylesheet" href="../blog1/theme/style.css" type="text/css" media="screen">
 </head>
 <body>
-<h1>Главная</h1>
-<a href="../blog1/index.php">Главная</a>&nbsp;|
-<a href="../blog1/editor.php">Консоль редактора</a>
+<h1>Home</h1>
+<a href="../blog1/index.php">Home</a>&nbsp;|
+<a href="../blog1/editor.php">Editor</a>
 <hr>
 	
 		<? foreach ($articles as $a): ?>
@@ -17,7 +17,8 @@
 				<a href="./article.php?id=<?=$a['article_id']?>">
 					<h3><?=$a['title']?></h3>
 				</a>
-				<p style="width: 70%;"><?=$a['content']?></p>	
+				<!-- <p><?=$a['content']?></p> -->	
+				<p><?php echo article_intro($a); ?></p>
 			</div>
 		<? endforeach; ?>
 	
